75957b21c917c35ba4c2d0b0f76a297919781f48
Lethe <37437842+0xlethe@users.noreply.github.com>
2023-05-25 14:53:16 +0900

    feat: init android compose

---
+++ app/src/main/java/com/moblie/programming/assignment/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.moblie.programming.assignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.moblie.programming.assignment.ui.theme.AssignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AssignmentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+class test {
+    companion object {
+        val value: Type
+        fun init() {
+            value = asdf''
+        }
+
+        fun get() {
+            return value
+        }
+    }
+}
+
+test.init()
+test.get()
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AssignmentTheme {
+        Greeting("Android")
+    }
+}
